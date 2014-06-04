17ee19b8283a214141bb899410b465fad8d5f06e
Stanley Ng <stanleyhlng@yahoo.com.hk>
2014-06-04 01:21:45 -0700

    initial checkin

---
+++ ios_tip_calculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  ios_tip_calculator
+//
+//  Created by Stanley Ng on 6/4/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
